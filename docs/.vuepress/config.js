5a97ab38ee282bdf3c9173bd16ac8038a64ef271
oDen7 <dengao99@126.com>
2022-06-12 19:02:16 +0800

    fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,147 +1,150 @@
-module.exports = {
-  lang: 'zh-CN',
-  title: 'oDen7的前端博客',
-  description: '个人学习记录,成长日记',
-  base:"/blog-md/",
+const { defaultTheme } = require('vuepress')
 
-  theme: '@vuepress/theme-default',
-  themeConfig: {
-    logo: 'https://vuejs.org/images/logo.png',
-    sidebar: [{
-        text: "前端",
-        collapsible: true,
-        children: [{
-            text: 'javascript',
+module.exports = {
+    lang: 'zh-CN',
+    title: 'oDen7的个人博客',
+    description: '个人学习记录,成长日记',
+    base:"/blog-md/",
+    theme: defaultTheme({
+        logo: 'https://vuejs.org/images/logo.png',
+        sidebar: [{
+            text: "前端",
             collapsible: true,
             children: [{
-                text: '变量',
-                children: ["/front-end/javascript/变量/README.md"]
+                text: 'javascript',
+                collapsible: true,
+                children: [
+                  {
+                    text: '变量',
+                    children: ["/front-end/javascript/变量/README.md"]
+                  },
+                  {
+                    text: '变量作用域内存',
+                    children: ['/front-end/javascript/变量作用域内存/README.md']
+                  },
+                  {
+                    text: '数据类型',
+                    children: ['/front-end/javascript/数据类型/README.md']
+                  },
+                  {
+                    text: '基本引用类型',
+                    children: ['/front-end/javascript/基本引用类型/README.md']
+                  },
+                  {
+                    text: '操作符',
+                    children: ['/front-end/javascript/操作符/README.md']
+                  },
+                 
+                  {
+                    text: '闭包',
+                    children: ['/front-end/javascript/闭包/README.md']
+                  },
+                  {
+                    text: '对象',
+                    children: ['/front-end/javascript/对象/README.md']
+                  },
+                  {
+                    text: '函数',
+                    children: ['/front-end/javascript/函数/README.md']
+                  },
+                  {
+                    text: '客户端检测',
+                    children: ['/front-end/javascript/客户端检测/README.md']
+                  },
+                  {
+                    text: '类',
+                    children: ['/front-end/javascript/类/README.md']
+                  },
+                  {
+                    text: '文档对象模型',
+                    children: ['/front-end/javascript/文档对象模型/README.md']
+                  },
+                  {
+                    text: '浏览器对象模型',
+                    children: ['/front-end/javascript/浏览器对象模型/README.md']
+                  },
+                  {
+                    text: '模块',
+                    children: ['/front-end/javascript/模块/README.md']
+                  },
+                  {
+                    text: '事件循环',
+                    children: ['/front-end/javascript/事件循环/README.md']
+                  },
+                  {
+                    text: "算法图解",
+                    children: ['/front-end/javascript/algorithm-diagram-javascript/README.md']
+                  },
+                  {
+                    text: "call,apply,bind",
+                    children: ['/front-end/javascript/callApplyBind/README.md']
+                  },
+                  {
+                    text: '双向数据绑定',
+                    children: ['/front-end/javascript/双向数据绑定/README.md']
+                  },
+                ],
+              },
+               {
+                text: 'vue',
+                collapsible: true,
+                children: []
+              }, {
+                text: 'react',
+                collapsible: true,
+                children: []
               },
               {
-                text: '变量作用域内存',
-                children: ['/front-end/javascript/变量作用域内存/README.md']
+                text: 'typescript',
+                collapsible: true,
+                children: [{
+                  text: 'tsc',
+                  children: ["/front-end/typescript/tsc/README.md"]
+                }]
               },
               {
-                text: '数据类型',
-                children: ['/front-end/javascript/数据类型/README.md']
+                text: 'webpack',
+                collapsible: true,
+                children: [{
+                  text: '构建vue',
+                  children: ["/front-end/webpack/constructVue/README.md"]
+                }]
               },
               {
-                text: '基本引用类型',
-                children: ['/front-end/javascript/基本引用类型/README.md']
-              },
-              {
-                text: '操作符',
-                children: ['/front-end/javascript/操作符/README.md']
-              },
-              {
-                text: '双向数据绑定',
-                children: ['/front-end/javascript/双向数据绑定/README.md']
-              },
-              {
-                text: '闭包',
-                children: ['/front-end/javascript/闭包/README.md']
-              },
-              {
-                text: '对象',
-                children: ['/front-end/javascript/对象/README.md']
-              },
-              {
-                text: '函数',
-                children: ['/front-end/javascript/函数/README.md']
-              },
-              {
-                text: '客户端检测',
-                children: ['/front-end/javascript/客户端检测/README.md']
-              },
-              {
-                text: '类',
-                children: ['/front-end/javascript/类/README.md']
-              },
-              {
-                text: '文档对象模型',
-                children: ['/front-end/javascript/文档对象模型/README.md']
-              },
-              {
-                text: '浏览器对象模型',
-                children: ['/front-end/javascript/浏览器对象模型/README.md']
-              },
-              {
-                text: '模块',
-                children: ['/front-end/javascript/模块/README.md']
-              },
-              {
-                text: '事件循环',
-                children: ['/front-end/javascript/事件循环/README.md']
-              },
-              {
-                text: "算法图解",
-                children: ['/front-end/javascript/algorithm-diagram-javascript/README.md']
-              },
-              {
-                text: "call,apply,bind",
-                children: ['/front-end/javascript/callApplyBind/README.md']
-              }
-            ],
-          }, {
-            text: 'vue',
-            collapsible: true,
-            children: []
-          }, {
-            text: 'react',
-            collapsible: true,
-            children: []
-          },
-          {
-            text: 'typescript',
-            collapsible: true,
-            children: [{
-              text: 'tsc',
-              children: ["/front-end/typescript/tsc/README.md"]
-            }]
-          },
-          // {
-          //   text: 'webpack',
-          //   collapsible: true,
-          //   children: [{
-          //     text: '构建vue',
-          //     children: ["/front-end/webpack/constructVue/README.md"]
-          //   }]
-          // },
-          {
-            text: 'nodejs',
-            collapsible: true,
-            children: [{
                 text: 'nodejs',
-                children: ["/front-end/nodejs/nodejs/README.md"]
-              },
-              {
-                text: '包',
-                children: ["/front-end/nodejs/包/README.md"]
-              }, {
-                text: '核心模块',
-                children: ["/front-end/nodejs/核心模块/README.md"]
-              }, {
-                text: '模块',
-                children: ["/front-end/nodejs/模块/README.md"]
-              }, {
-                text: '内存管理',
-                children: ["/front-end/nodejs/内存管理/README.md"]
-              }, {
-                text: '异步IO',
-                children: ["/front-end/nodejs/异步IO/README.md"]
-              }, {
-                text: '异步编程',
-                children: ["/front-end/nodejs/异步编程/README.md"]
+                collapsible: true,
+                children: [{
+                    text: 'nodejs',
+                    children: ["/front-end/nodejs/nodejs/README.md"]
+                  },
+                  {
+                    text: '包',
+                    children: ["/front-end/nodejs/包/README.md"]
+                  }, {
+                    text: '核心模块',
+                    children: ["/front-end/nodejs/核心模块/README.md"]
+                  }, {
+                    text: '模块',
+                    children: ["/front-end/nodejs/模块/README.md"]
+                  }, {
+                    text: '内存管理',
+                    children: ["/front-end/nodejs/内存管理/README.md"]
+                  }, {
+                    text: '异步IO',
+                    children: ["/front-end/nodejs/异步IO/README.md"]
+                  }, {
+                    text: '异步编程',
+                    children: ["/front-end/nodejs/异步编程/README.md"]
+                  }
+                ]
               }
             ]
-          }
+          },
+          // {
+          //   text: "",
+          //   link: "",
+          //   children: []
+          // },
         ]
-      },
-      // {
-      //   text: "",
-      //   link: "",
-      //   children: []
-      // },
-    ]
+    })
   }
-}
