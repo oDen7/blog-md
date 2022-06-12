9d780204a9a790f98017c6bb7af6975a3a6de96f
oDen7 <dengao99@126.com>
2022-06-12 16:37:44 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:oDen7/front-end/.git main
+git push -f git@github.com:oDen7/front-end-blog-md.git main
+
 
 cd -
